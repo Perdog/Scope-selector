c6061ea8328ae9b75a0d7452853e6514cf0086ed
Perdog <jacob_perry@hotmail.ca>
2018-06-05 21:45:01 -0400

    Analytics and page clean up

--- code.js
+++ code.js
@@ -34,30 +34,26 @@ $(document).ready(function() {
 		/*
 		https://login.eveonline.com/oauth/authorize
 		?response_type=token
-		&redirect_uri=https://perdog.github.io/FEDUP-Member-Activity/index.html?callback
+		&redirect_uri=test.com
 		&client_id=d39d34e683aa48c2b535b099c3dc404a
-		&scope=publicData esi-corporations.read_corporation_membership.v1 esi-characters.read_corporation_roles.v1 esi-corporations.track_members.v1 esi-corporations.read_titles.v1 esi-corporations.read_fw_stats.v1
+		&scope=esi-corporations.read_corporation_membership.v1 esi-characters.read_corporation_roles.v1 esi-corporations.track_members.v1 esi-corporations.read_titles.v1 esi-corporations.read_fw_stats.v1
 		&state=membertracker
 		*/
 		type = parseSearch("response_type");
 		redirect = parseSearch("redirect_uri");
 		clientid = parseSearch("client_id");
-		//var nonTokenVersionOfClientID = parseSearch("");
 		scope = parseSearch("scope");
 		state = parseSearch("state");
 		
-		// If all vars are true, the page has been called properly.
-		if (type && redirect && (clientid || true/*Lookup the refresh token equivilant*/) && scope && state) {
-			console.log(scope);
+		// If all vars exist, the page has been called properly.
+		if (type && redirect && clientid && scope && state) {
 			var scopes = scope.split(" ");
-			console.log(scopes);
+			
 			for (var i = 0; i < scopes.length; i++) {
 				var esi = scopes[i];
-				console.log(esi);
 				var elem = $("div[id=\""+esi+"\"]");
-				console.log(elem);
+				
 				elem.show();
-				console.log(elem);
 			}
 			
 			var charscope = $("#char-scopes").find(".grid").filter(":visible");
@@ -76,18 +72,18 @@ $(document).ready(function() {
 		else {
 			var missing = "";
 			if (!type)
-				missing += "'response_type' needs to be set in the URL. This is used by CCP to know what kind of token to return to you.\n";
+				missing += "'response_type' needs to be set in the URL. This is used by CCP to know what kind of token to return to you. Can be 'token' or 'code'. 'code' will give a you a token that you must verify futher with CCP, but will give you access to a 'refresh token', while 'token' gives you a code that works right away, but is only good for 30 minutes.\n\n";
 			if (!redirect)
-				missing += "'redirect_uri' is not set. How is CCP supposed to know who to send the token back to??\n";
+				missing += "'redirect_uri' is not set. How is CCP supposed to know who to send the token back to??\n\n";
 			if (!clientid)
-				missing += "";
+				missing += "'client_id' was not found. This is how CCP knows what app is requesting access.\n\n";
 			if (!scope)
-				missing += "'scope' is kind of neccassary... I mean that's the whole point of this page..\n";
+				missing += "'scope' is kind of neccassary... I mean that's the whole point of this page...\n\n";
 			if (!state)
-				missing += "'state' is highly recommended for auth requests. It lets you make sure no one has tampered with your request.\n";
+				missing += "'state' is highly recommended for auth requests. It lets you make sure no one has tampered with your request. As such, I'm making it required for use with this website, since it opens up possibilities for interception.\n\n";
 			
 			console.log(missing);
-			// Load 'missing' onto the page somewhere.
+			// Load 'missing' onto the page somewhere so that people can yell at other devs.
 		}
 		
 	}
@@ -129,7 +125,8 @@ function parseSearch(variable) {
             return decodeURIComponent(pair[1]);
         }
     }
-    console.log('Search variable %s not found', variable);
+    console.log('Search variable "%s" not found', variable);
+	return false;
 }
 
 function parseHash(variable) {
@@ -142,5 +139,6 @@ function parseHash(variable) {
         }
     }
     console.log('Hash variable %s not found', variable);
+	return false;
 }
 
